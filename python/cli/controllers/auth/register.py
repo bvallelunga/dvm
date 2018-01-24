e8562648379dbb4e7272a9141c9ffcf0f6568cfd
Brian Vallelunga <vallelungabrian@gmail.com>
2018-01-24 00:50:11 -0800

    small changes to register

--- python/cli/controllers/auth/register.py
+++ python/cli/controllers/auth/register.py
@@ -11,16 +11,19 @@ class AuthRegisterController(CementBaseController):
     usage = 'dvm auth register [arguments...]'
     description = 'Create new user account linked to your wallet address.'
     arguments = [
-      (['--name', '-n'], dict(action='store', help="OPTIONAL: Full Name", dest="name")),
+      (['--name', '-n'], dict(action='store', help="REQUIRED: Full Name", dest="name")),
       (['--email', '-e'], dict(action='store', help="REQUIRED: Email Address", dest="email")),
       (['--password', '-p'], dict(action='store', help="REQUIRED: Password", dest="password")),
       (['--wallet', '-w'], dict(action='store', help="REQUIRED: DOP Wallet Address", dest="wallet")),
-      (['--endpoint', '-ep'], dict(action='store', help="OPTIONAL: Endpoint for Doppler to contact your server", dest="endpoint")),
+      (['--endpoint', '-ep'], dict(action='store', help="REQUIRED: Endpoint for Doppler to contact your server", dest="endpoint")),
     ]
    
   
   @expose(hide=True)
-  def default(self):  
+  def default(self):
+    if not self.app.pargs.endpoint:
+      return self.app.log.error("Field 'endpoint' is required")
+    
     user = UserService.register(
       name = self.app.pargs.name,
       email = self.app.pargs.email,
@@ -31,12 +34,11 @@ class AuthRegisterController(CementBaseController):
     if not user: return
     self.app.store.set("access-token", user.wallet)
     provider = ProviderService.register(
-      endpoint = self.app.pargs.endpoint 
+      endpoint = self.app.pargs.endpoint
     )
     
     if not provider: return
     self.app.store.set("provider", provider.id)
     self.app.store.set("apps", {})
     self.app.log.info("Account created, here is your access token: " + user.wallet)
-    self.app.log.info("Your access token has been saved here: " + self.app.store.filePath) 
     self.app.log.info("Provider attached to your user account")
