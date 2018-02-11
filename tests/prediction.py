563286a3617bb2c0c29ed7d11b5cbffeedd7fab2
Brian Vallelunga <vallelungabrian@gmail.com>
2018-02-11 01:26:18 -0800

    added prediction test

---
+++ tests/prediction.py
@@ -0,0 +1,32 @@
+# Install the Python Requests library:
+# `pip install requests`
+
+import requests
+import json
+import io, base64
+
+
+def testImage():
+  TEST_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"
+  response = requests.get(TEST_IMG_URL)
+  img_stream = io.BytesIO(response.content)
+  return base64.encodestring(img_stream.read()).decode("utf-8")
+
+
+try:
+    response = requests.post(
+        url="https://api.doppler.market/v1/apps/4/models/8/prediction",
+        headers={
+            "Content-Type": "application/json; charset=utf-8",
+            "access-token": "okximez4eaz53cpoikvalfswkrog9qgs4rfbwfp0",
+        },
+        data=json.dumps({
+            "image": testImage()
+        })
+    )
+
+    print(json.dumps(response.json()["output"], indent=2))
+except requests.exceptions.RequestException:
+    print('HTTP Request failed')
+
+
