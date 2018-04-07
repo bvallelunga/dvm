65f61d97b25f27a04b2b381eb131570a1a5d6e57
Phillip Chao <phillipchao531@gmail.com>
2018-04-07 15:28:45 -0700

    changed registration flow to open browser. closes #34

--- cli/controllers/auth/register.py
+++ cli/controllers/auth/register.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from cement.core.controller import CementBaseController, expose
 from cli.utils.services.user import UserService
 from cli.utils.services.provider import ProviderService
+import webbrowser
 
 
 class AuthRegisterController(CementBaseController):
@@ -9,39 +10,11 @@ class AuthRegisterController(CementBaseController):
     label = 'register'
     stacked_on = 'auth'
     stacked_type = 'nested'
-    usage = 'dvm register [arguments...]'
-    description = 'Create new user account linked to your wallet address.'
-    arguments = [
-      (['--name', '-n'], dict(action='store', help="REQUIRED: Full Name", dest="name")),
-      (['--email', '-e'], dict(action='store', help="REQUIRED: Email Address", dest="email")),
-      (['--password', '-p'], dict(action='store', help="REQUIRED: Password", dest="password")),
-      (['--wallet', '-w'], dict(action='store', help="REQUIRED: DOP Wallet Address", dest="wallet")),
-      (['--endpoint', '-ep'], dict(action='store', help="REQUIRED: Endpoint for Doppler to contact your server", dest="endpoint")),
-    ]
-   
-  
+    usage = 'dvm register'
+    description = 'Create a new user account'
+
+
   @expose(hide=True)
   def default(self):
-    if not self.app.pargs.endpoint:
-      self.app.log.error("Field 'endpoint' is required")
-      self.app.args.print_help()
-      return
-    
-    user = UserService.register(
-      name = self.app.pargs.name,
-      email = self.app.pargs.email,
-      password = self.app.pargs.password,
-      wallet = self.app.pargs.wallet
-    )
-    
-    if not user: return self.app.args.print_help()
-    self.app.store.set("access-token", user.wallet)
-    provider = ProviderService.register(
-      endpoint = self.app.pargs.endpoint
-    )
-    
-    if not provider: return self.app.args.print_help()
-    self.app.store.set("provider", provider.id)
-    self.app.store.set("apps", {})
-    self.app.log.info("Account created, here is your access token: " + user.wallet)
-    self.app.log.info("Provider attached to your user account")
+      self.app.log.info("Welcome to Doppler, redirecting you to https://doppler.market/register to create an account.")
+      webbrowser.open_new_tab("https://doppler.market/register")
