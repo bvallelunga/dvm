00625963f6b6c1da8746f3a91ad6a3f1012c702f
Brian Vallelunga <vallelungabrian@gmail.com>
2018-05-17 01:54:56 -0700

    udpated bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -5,6 +5,7 @@ APPS=$2
 
 # DO NOT TOUCH
 IP=$(curl http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+dvm server --kill
 dvm login --access-token="$ACCESS_TOKEN" --endpoint="http://$IP"
 
 for app in $APPS
